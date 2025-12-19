78eb95a86c3348f78359f786c9f793e7c90e1fdc
baghonggyun44-creator <baghonggyun44@gmail.com>
2025-12-19 22:00:23 +0900

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+  NEWSPIC_URL: "https://m.newspic.kr/main?category=hot",
+  MAX_CANDIDATES: 30,
+  POST_COUNT: 5,
+  MIN_SCORE: 3,
+
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+
+  STATE_PATH: "data/state.json"
+};
