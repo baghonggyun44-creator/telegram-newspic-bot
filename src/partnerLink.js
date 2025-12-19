1afd93975b0b9a7bf529fe490d83278e34bfe8cf
baghonggyun44-creator <baghonggyun44@gmail.com>
2025-12-19 23:40:51 +0900

    Update partnerLink.js

--- src/partnerLink.js
+++ src/partnerLink.js
@@ -1,6 +1,20 @@
-export function toPartnerLink(originalUrl) {
-  return originalUrl.replace(
-    "https://m.newspic.kr",
-    "https://im.newspic.kr"
+import axios from "axios";
+
+export async function createPartnerLink({ nid, pn, cp }) {
+  const url = "https://m.newspic.kr/api/partners/link";
+
+  const params = new URLSearchParams();
+  params.append(
+    "query",
+    `?nid=${nid}&pn=${pn}&cp=${cp}&utm_medium=affiliate&utm_campaign=${nid}&utm_source=np${cp}`
   );
+  params.append("requestKey", cp);
+
+  const res = await axios.post(url, params, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  return res.data; // 👉 https://im.newspic.kr/xxxx
 }
