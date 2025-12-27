9d0fcd036a6892af9578976da89828500fb86fbe
baghonggyun44-creator <baghonggyun44@gmail.com>
2025-12-28 00:03:39 +0900

    Update dedupStore.js

--- src/dedupStore.js
+++ src/dedupStore.js
@@ -4,24 +4,41 @@ const STORE_PATH = "./posted.json";
 
 function loadStore() {
   if (!fs.existsSync(STORE_PATH)) {
-    fs.writeFileSync(STORE_PATH, JSON.stringify([]));
+    fs.writeFileSync(
+      STORE_PATH,
+      JSON.stringify(
+        {
+          postedIds: [],
+          lastPostedAt: 0
+        },
+        null,
+        2
+      )
+    );
   }
+
   return JSON.parse(fs.readFileSync(STORE_PATH, "utf-8"));
 }
 
-function saveStore(data) {
-  fs.writeFileSync(STORE_PATH, JSON.stringify(data, null, 2));
+function saveStore(store) {
+  fs.writeFileSync(STORE_PATH, JSON.stringify(store, null, 2));
 }
 
 export function isDuplicate(id) {
   const store = loadStore();
-  return store.includes(id);
+  return store.postedIds.includes(id);
 }
 
 export function savePosted(id) {
   const store = loadStore();
-  if (!store.includes(id)) {
-    store.push(id);
+  if (!store.postedIds.includes(id)) {
+    store.postedIds.push(id);
+    store.lastPostedAt = Date.now();
     saveStore(store);
   }
+}
+
+export function canPostNow(intervalMs) {
+  const store = loadStore();
+  return Date.now() - store.lastPostedAt >= intervalMs;
 }
